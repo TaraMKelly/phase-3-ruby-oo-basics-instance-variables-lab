8bbac112ebedb136c6a6330fd07dfbdc28479d95
TaraMKelly <tara.kelly16@gmail.com>
2021-08-09 21:23:43 -0400

    commit tree bd88ac8c3b61428b15dfd72c720933446dbc018e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    def name=(dog_name) #assigns/sets the dog's name
+        @this_dogs_name = dog_name
+    end
 
+    def name #responsible for reporting/getting the dog's name
+        @this_dogs_name 
+    end
 end
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
+
+# we want to be able to do this:
+#     lassie = Dog.new -> creating a new Dog instance
+#     lassie.name = "Lassie" -> assigning it a name 
